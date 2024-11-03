9e7bb6c941a2d3a298ef539868d7b6b1ef0d7f5d
Dung Dang <jim.dangquoc706@gmail.com>
2024-11-03 12:12:34 +0700

    redirect from home to article page

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,5 +1,10 @@
+import { redirect } from '@sveltejs/kit';
+
 /** @type {import('@sveltejs/kit').Handle} */
 export async function handle({ event, resolve }) {
   console.info(`${event.request.method}: ${event.url.pathname}`);
+  if (event.url.pathname === "/") {
+    redirect(302, "/posts");
+  }
   return await resolve(event);
 }
