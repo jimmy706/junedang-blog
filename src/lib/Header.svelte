17a8b4fac3218b0c1f49364211a7c12f7b71a8cd
Dung Dang <jim.dangquoc706@gmail.com>
2024-11-07 00:14:08 +0700

    complete about page

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -11,14 +11,23 @@
     {
       name: "Home",
       href: "/",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>`,
     },
     {
       name: "About",
       href: "/about",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>`
     },
     {
       name: "Articles",
       href: "/posts",
+      icon: `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 0 1-2.25 2.25M16.5 7.5V18a2.25 2.25 0 0 0 2.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 0 0 2.25 2.25h13.5M6 7.5h3v3H6v-3Z" />
+</svg>`,
     },
   ];
 </script>
@@ -58,7 +67,7 @@
         </svg>
       </button>
     </div>
-    <div class="hidden lg:flex lg:gap-x-8">
+    <div class="hidden lg:flex lg:gap-x-5">
       <div class="relative hidden">
         <button
           type="button"
@@ -398,7 +407,7 @@
                 >
               </div>
             </div>
-            
+
             {#each menuItems as item}
               <a
                 href={item.href}
